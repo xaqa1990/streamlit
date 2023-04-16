6472ff7a199ae9e5078d05b7fd906f50e2d54e34
xaqa1990 <anqi.xu@utah.edu>
2023-04-16 12:19:19 -0400

    Update all_charts.py

--- all_charts.py
+++ all_charts.py
@@ -19,6 +19,7 @@ st.pyplot()
 
 # Transpose the dataset using df.transpose() if needed
 dfpct = dfpct.transpose()
+dfpct.plot()
 st.pyplot()
 
 # Order the x-axis if needed using df.sort_index()
@@ -51,3 +52,60 @@ plt.title("Car Percentage")
 #Add/Change legend using plt.legend(labels = ["line 1", "line 2"])
 st.pyplot()
 
+
+
+df_sales = pd.read_csv(path + 'cars_sales.csv', index_col='Country')
+print(df_sales)
+
+# Show the bar chart of all sales in Germany using df.plot(kind = "bar"/"barh")
+df_sales.loc["Germany"].plot(kind = "bar")
+st.pyplot()
+
+df_sales.loc["Germany"].plot(kind = "bar", color = ["red", "yellow", "green", "blue"])
+st.pyplot()
+
+df_sales.loc["Germany"].plot(kind = "barh")
+st.pyplot()
+
+# Show the bar chart for all countries in 2020
+df_sales.loc[:,"2020"].plot(kind = "bar")
+st.pyplot()
+
+# Show the bar chart for all countries in 2020 with sales ordered
+df_sales.loc[:,"2020"].sort_values().plot(kind = "bar")
+st.pyplot()
+
+# Show the cluster bar chart for Germany, Norway, France
+df_sales
+df_sales.loc[["Germany", "Norway", "France"]].plot(kind = "bar")
+st.pyplot()
+
+df_sales=df_sales.transpose()
+df_sales[["Germany", "Norway", "France"]].plot(kind = "bar")
+
+st.pyplot()
+
+# Show the stacked bar chart for Germany, Norway, France
+df_sales[["Germany", "Norway", "France"]].plot(kind = "bar", stacked = True)
+st.pyplot()
+
+
+# Show the sales distrubution in year 2020
+
+df_sales.loc["2020"].plot(kind = "pie")
+st.pyplot()
+
+df_sales["France"].plot(kind = "pie")
+st.pyplot()
+
+# To show the percentage of each category using df.plot(kind = "pie", autopct = "%.1f%%")
+'''
+autopct = '%.1f' # display the percentage value to 1 decimal place
+autopct = '%.2f' # display the percentage value to 2 decimal places
+autopct = '%.1f%%' # display the percentage value to 1 decimal places with the percentage sign
+autopct = '%.2f%%' # display the percentage value to 2 decimal places with the percentage sign
+'''
+
+
+# To show the absolute value of each category (optional)
+#df.plot(kind = "pie", autopct = lambda p: '{:.0f}'.format(p * sum(df) / 100))
